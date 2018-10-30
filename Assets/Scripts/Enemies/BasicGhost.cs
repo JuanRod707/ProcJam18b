6d529f39e083ec0b528a7dca0e55d71fa88e9693
Juancho <juan.rod707@gmail.com>
2018-10-30 02:49:33 -0300

    Added basic AI

---
+++ Assets/Scripts/Enemies/BasicGhost.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+    public class BasicGhost : MonoBehaviour
+    {
+        public float EngageDistance;
+        public float MinimumDistance;
+
+        private Movement.Movement movement;
+        private Transform character;
+        private EnemyGun[] weapons;
+
+        private bool IsInDistance => Vector3.Distance(transform.position, character.position) < EngageDistance;
+        private bool IsTooClose => Vector3.Distance(transform.position, character.position) < MinimumDistance;
+
+        void Start()
+        {
+            movement = GetComponent<Movement.Movement>();
+            character = GameObject.FindWithTag("Player").transform;
+            weapons = GetComponentsInChildren<EnemyGun>();
+        }
+
+        void Update()
+        {
+            if (IsInDistance)
+            {
+                LookAtCharacter();
+
+                if (IsTooClose)
+                    movement.MoveBackwards();
+                else
+                    movement.MoveForward();
+
+                foreach (var w in weapons)
+                    w.Fire();
+            }
+        }
+
+        void LookAtCharacter()
+        {
+            var charPosition = transform.InverseTransformPoint(character.position);
+            if (charPosition.x > 0)
+                movement.TurnRight();
+            else
+                movement.TurnLeft();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyGun : MonoBehaviour
+    {
+        public GameObject Projectile;
+        public Transform FiringPosition;
+        public float RateOfFire;
+        
+        private bool isCiclying;
+
+        public void Fire()
+        {
+            if (!isCiclying)
+                Shoot();
+        }
+
+        IEnumerator Cycle()
+        {
+            isCiclying = true;
+            yield return new WaitForSeconds(RateOfFire);
+            isCiclying = false;
+        }
+
+        void Shoot()
+        {
+            Instantiate(Projectile, FiringPosition.transform.position, FiringPosition.rotation);
+            StartCoroutine(Cycle());
+        }
+    }
+}
